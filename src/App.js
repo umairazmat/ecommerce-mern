ab652e64d3261d4db6da8b42ff853340c66cb57b
Umair Azmat <umairazmat8260.com>
2023-07-20 06:13:13 +0500

    Product list and categories added 🔥

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import ProductList from './features/product/ProductList';
 
 function App() {
   return (
     <div className="App">
-    
+    <ProductList/>
     </div>
   );
 }
