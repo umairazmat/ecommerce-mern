bb6451baf946b8068d3c50c669a45878f793ff2e
Umair Azmat <umairazmat8260.com>
2023-07-22 21:10:39 +0500

    Routing Done 🚀

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home />),
+  },
+  {
+    path: "/login",
+    element: (<LoginPage />),
+  },
+  {
+    path: "/signup",
+    element: (<SignUpPage />),
+  }
+]);
+
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+       <RouterProvider router={router} />
   );
 }
 
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import SignUp from '../features/auth/components/SignUp'
+
+export default function SignUpPage() {
+  return (
+   <SignUp/>
+  )
+}
